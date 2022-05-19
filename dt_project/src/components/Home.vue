4f402b8f19f232bbc0d3fb0c51961197b5aaa19a
Ilie_Chirila <ilieraduchirila@gmail.com>
2022-05-20 00:05:56 +0300

    Video 15 done. Careful when using Async and Await !!

--- dt_project/src/components/Home.vue
+++ dt_project/src/components/Home.vue
@@ -3,9 +3,19 @@
 <h1>
     Hello {{username}}, Welcome on Home page
 </h1>
-<table>
+<table border="1">
+    <tr>
+        <td>Id</td>
+        <td>Name</td>
+        <td>Date</td>
+        <td>Time</td>
+
+    </tr>
     <tr v-for="item in show" :key="item.id">
+        <td>{{item.id}}</td>
         <td>{{item.name}}</td>
+        <td>{{item.date}}</td>
+        <td>{{item.time}}</td>
     </tr>
 </table>
 </template>
@@ -26,16 +36,23 @@ export default {
     components:{
         Header
     },
-      mounted(){
+      async mounted(){
         let user = localStorage.getItem('user-info');
         this.username = JSON.parse(user).username;
         if(!user){
             this.$router.push({name:'SignUp'})
         }
-        let result = axios.get("http://localhost:3000/show");
+        let result = await axios.get("http://localhost:3000/show");
         console.warn(result)
         this.show = result.data;
     }
     
 }
 </script>
+
+<style scoped>
+    td{
+        width:160px;
+        height: 50;
+    }
+</style>
