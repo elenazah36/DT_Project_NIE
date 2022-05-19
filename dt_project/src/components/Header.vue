6227181024792373d9ae22590c9d16802ebbcd2e
Ilie_Chirila <ilieraduchirila@gmail.com>
2022-05-19 23:03:53 +0300

    Basic navbar + logout. Vid 12 done

---
+++ dt_project/src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+   <div class="nav">
+       <a href="#">Home</a>
+       <a href="#">Add Show</a>
+       <a href="#">Update Show</a>
+       <a v-on:click="logout" href="#">Logout</a>
+   </div>
+</template>
+
+<script>
+
+export default {
+    //eslint-disable-next-line
+    name:'Header',
+    methods:{
+        logout(){
+            localStorage.clear();
+            this.$router.push({name:'Login'})
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+/* Navbar style basic color */
+.nav{
+    background-color:#333;
+    overflow: hidden;
+}
+/* Navbar anchors style */
+.nav a{
+    float:left;
+    color: #f2f2f2;
+    padding:16px 14px;
+    text-align: center;
+    font-size: 17 px;
+    text-decoration: none;
+    margin-right:5px;
+}
+/* Navbar anchors style when hovering */
+.nav a:hover{
+    background-color: #ddd;
+    color:aqua
+}
+</style>
--- dt_project/src/components/Home.vue
+++ dt_project/src/components/Home.vue
@@ -1,12 +1,17 @@
 <template>
+<Header />
 <h1>
     Hello User, Welcome on Home page
 </h1>
 </template>
 <script>
+import Header from './Header.vue'
 export default {
     //eslint-disable-next-line
     name: 'Home', 
+    components:{
+        Header
+    },
       mounted(){
         let user = localStorage.getItem('user-info');
         if(!user){
